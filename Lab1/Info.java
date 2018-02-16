90c31b008cbaae13a798ccd273397eb3f55c31f7
Aleksey Goravskiy <32121103+southHub@users.noreply.github.com>
2018-02-16 08:58:26 +0200

    Add files via upload

---
+++ Lab1/Info.java
@@ -0,0 +1,16 @@
+
+/*
+Laba 1
+*/
+
+public class Info
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Name:\t" + "Aleksey Goravskiy");
+		System.out.println("Group:\t" + "Pois1709v2");
+		System.out.println("Work:\t" + "Design Engineer");
+		System.out.println("Hobby:\t" + "Getting new knowledge, many types of sports (my favorite is basketboll), traveling and coffee =)");
+	}
+}
+
