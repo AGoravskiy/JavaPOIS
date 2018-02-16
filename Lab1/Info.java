5bd4c4fff5af6254f453cf08ddde3680d75614f7
Aleksey Goravskiy <32121103+southHub@users.noreply.github.com>
2018-02-16 14:33:24 +0200

    Update Info.java

--- Lab1/Info.java
+++ Lab1/Info.java
@@ -10,7 +10,7 @@ public class Info
 		System.out.println("Name:\t" + "Aleksey Goravskiy");
 		System.out.println("Group:\t" + "Pois1709v2");
 		System.out.println("Work:\t" + "Design Engineer");
-		System.out.println("Hobby:\t" + "Getting new knowledge, many types of sports (my favorite is basketboll), traveling and coffee =)");
+		System.out.println("Hobby:\t" + "Getting new knowledge, many types of sports (my favorite is basketboll), traveling and coffee \u0001");
 	}
 }
 
